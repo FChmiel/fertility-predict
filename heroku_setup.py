9e18296cdddf037ca46c2a784ec553c785475beb
fpc <fpc@it-innovation.soton.ac.uk>
2020-06-10 09:51:43 +0100

    Updates to disclaimer and rename application

--- heroku_setup.py
+++ heroku_setup.py
@@ -1,4 +1,4 @@
 import os
 with open(os.path.join('Procfile'), "w") as file1:
-    toFile = 'web: sh setup.sh && streamlit run main.py'
+    toFile = 'web: sh setup.sh && streamlit run fertility-predict.py'
     file1.write(toFile)
